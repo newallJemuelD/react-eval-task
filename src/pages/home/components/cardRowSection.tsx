197eac79e4df1673d1495040d2845538d045a0b9
Newall.J <newall.j@msf.ad>
2024-07-10 18:01:06 +0530

    code optimisation

--- src/pages/home/components/cardRowSection.tsx
+++ src/pages/home/components/cardRowSection.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 
 import './components.css'
-import { useNavigate } from "react-router-dom";
 
 interface CardContent {
     title: string;
